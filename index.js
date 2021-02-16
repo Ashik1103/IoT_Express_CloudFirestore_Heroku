85efd7d2fa7468e0bf3f0dd744d2ee05ab0a70b0
Ashik1103 <hajaashik11032002@gmail.com>
2021-02-16 12:19:24 +0530

    second commit

--- index.js
+++ index.js
@@ -14,47 +14,6 @@ app.get('/', (req, res) => {
     '<h1>Tes Express & Firebase Cloud Firestore</h1><ul><li><p><b>GET /data/esp8266</b></p></li><li><p><b>GET /data/esp32</b></p></li><li><p><b>GET /data/mkr1000</b></p></li><li><p><b>POST /data/esp8266</b>  => {suhu, lembab, analog}</p></li><li><p><b>POST /data/esp32</b>  => {suhu, lembab, analog}</p></li><li><p><b>POST /data/mkr1000</b>  => {suhu, lembab, analog}</p></li></ul>')
 })
 
-app.get('/data/esp8266', (req, res) => {
-  const db = fire.firestore();
-    db.settings({
-      timestampsInSnapshots: true
-    });
-    var wholeData = []
-	db.collection('lin_esp8266').orderBy('waktu', 'asc').get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        // console.log(doc.id, '=>', doc.data());
-        // console.log(doc.data().name + doc.data().age);
-        // console.log(doc.data());
-        wholeData.push(doc.data())
-      });
-      console.log(wholeData)
-      res.send(wholeData)
-    })
-    .catch(error => {
-      console.log('Error!', error);
-  })
-})
-
-app.post('/data/esp8266', (req, res)=>{
-  const db = fire.firestore();
-	db.settings({
-      timestampsInSnapshots: true
-    });
-    db.collection('lin_esp8266').add({
-      suhu: req.body.suhu,
-      lembab: req.body.lembab,
-      analog: req.body.analog,
-      waktu: new Date()
-    });
-    res.send({
-      suhu: req.body.suhu,
-      lembab: req.body.lembab,
-      analog: req.body.analog,
-      waktu: new Date(),
-      status: 'POST data sukses!'
-  })
-})
 
 app.get('/data/esp32', (req, res) => {
   const db = fire.firestore();
@@ -62,7 +21,7 @@ app.get('/data/esp32', (req, res) => {
       timestampsInSnapshots: true
     });
     var wholeData = []
-	db.collection('lin_esp32').orderBy('waktu', 'asc').get()
+	db.collection('rover').get()
     .then(snapshot => {
       snapshot.forEach(doc => {
         // console.log(doc.id, '=>', doc.data());
@@ -83,11 +42,11 @@ app.post('/data/esp32', (req, res)=>{
 	db.settings({
       timestampsInSnapshots: true
     });
-    db.collection('lin_esp32').add({
-      suhu: req.body.suhu,
-      lembab: req.body.lembab,
-      analog: req.body.analog,
-      waktu: new Date()
+    db.collection('rover').add({
+      temperature: req.body.suhu,
+      ph_value: req.body.lembab,
+      turbidity: req.body.analog,
+      Date: new Date()
     });
     res.send({
       suhu: req.body.suhu,
@@ -120,25 +79,25 @@ app.get('/data/mkr1000', (req, res) => {
   })
 })
 
-app.post('/data/mkr1000', (req, res)=>{
-  const db = fire.firestore();
-	db.settings({
-      timestampsInSnapshots: true
-    });
-    db.collection('lin_mkr1000').add({
-      suhu: req.body.suhu,
-      lembab: req.body.lembab,
-      analog: req.body.analog,
-      waktu: new Date()
-    });
-    res.send({
-      suhu: req.body.suhu,
-      lembab: req.body.lembab,
-      analog: req.body.analog,
-      waktu: new Date(),
-      status: 'POST data sukses!'
-  })
-})
+// app.post('/data/mkr1000', (req, res)=>{
+//   const db = fire.firestore();
+// 	db.settings({
+//       timestampsInSnapshots: true
+//     });
+//     db.collection('lin_mkr1000').add({
+//       suhu: req.body.suhu,
+//       lembab: req.body.lembab,
+//       analog: req.body.analog,
+//       waktu: new Date()
+//     });
+//     res.send({
+//       suhu: req.body.suhu,
+//       lembab: req.body.lembab,
+//       analog: req.body.analog,
+//       waktu: new Date(),
+//       status: 'POST data sukses!'
+//   })
+// })
 
 app.listen(PORT, () => {
   console.log(`Listening on ${ PORT }`)
