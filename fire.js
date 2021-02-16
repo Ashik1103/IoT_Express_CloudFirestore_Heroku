8999c8265a29d0d4428aa2e236c683b2369bcae7
Ashik1103 <hajaashik11032002@gmail.com>
2021-02-16 11:22:07 +0530

    initial commit

--- fire.js
+++ fire.js
@@ -1,12 +1,15 @@
 var firebase = require('firebase')
 // change lines below with your own Firebase snippets!
 var config = {
-  apiKey: "AIzaSyDSLRypcYx2yHSqpVXJMBrzYw1xAHZtgYI",
-  authDomain: "test-iot-esp.firebaseapp.com",
-  databaseURL: "https://test-iot-esp.firebaseio.com",
-  projectId: "test-iot-esp",
-  storageBucket: "test-iot-esp.appspot.com",
-  messagingSenderId: "467772798973"
+    apiKey: "AIzaSyDf8u_VHAswod8ioK7wqPdNvT_-BM4Y7Jo",
+    authDomain: "roverbay-b057d.firebaseapp.com",
+    databaseURL: "https://roverbay-b057d-default-rtdb.firebaseio.com",
+    projectId: "roverbay-b057d",
+    storageBucket: "roverbay-b057d.appspot.com",
+    messagingSenderId: "453827648901",
+    appId: "1:453827648901:web:183370000f0f6218cef8ec",
+    measurementId: "G-7XZ8LPK7HH"
 };
 const fire = firebase.initializeApp(config);
 module.exports = fire;
+
