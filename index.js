43f5110f4125385f291a7480dab2a9f9240e8575
Ashik1103 <hajaashik11032002@gmail.com>
2021-02-19 12:53:17 +0530

    post commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/data/esp32', (req, res) => {
 	db.collection('rover').get()
     .then(snapshot => {
       snapshot.forEach(doc => {
-        // console.log(doc.id, '=>', doc.data());
+        console.log(doc.id, '=>', doc.data());
         // console.log(doc.data().name + doc.data().age);
         // console.log(doc.data());
         wholeData.push(doc.data())
@@ -43,61 +43,19 @@ app.post('/data/esp32', (req, res)=>{
       timestampsInSnapshots: true
     });
     db.collection('rover').add({
-      temperature: req.body.suhu,
-      ph_value: req.body.lembab,
-      turbidity: req.body.analog,
+      temperature: req.body.temp,
+      ph_value: req.body.ph_val,
+      turbidity: req.body.turbi,
       Date: new Date()
     });
     res.send({
-      suhu: req.body.suhu,
-      lembab: req.body.lembab,
-      analog: req.body.analog,
-      waktu: new Date(),
-      status: 'POST data sukses!'
+      temperature: req.body.temp,
+      ph_value: req.body.ph_val,
+      turbidity: req.body.turbi,
+      Date: new Date(), 
+      status: 'POST data success!'
   })
 })
-
-app.get('/data/mkr1000', (req, res) => {
-  const db = fire.firestore();
-    db.settings({
-      timestampsInSnapshots: true
-    });
-    var wholeData = []
-	db.collection('lin_mkr1000').orderBy('waktu', 'asc').get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        // console.log(doc.id, '=>', doc.data());
-        // console.log(doc.data().name + doc.data().age);
-        // console.log(doc.data());
-        wholeData.push(doc.data())
-      });
-      console.log(wholeData)
-      res.send(wholeData)
-    })
-    .catch(error => {
-      console.log('Error!', error);
-  })
-})
-
-// app.post('/data/mkr1000', (req, res)=>{
-//   const db = fire.firestore();
-// 	db.settings({
-//       timestampsInSnapshots: true
-//     });
-//     db.collection('lin_mkr1000').add({
-//       suhu: req.body.suhu,
-//       lembab: req.body.lembab,
-//       analog: req.body.analog,
-//       waktu: new Date()
-//     });
-//     res.send({
-//       suhu: req.body.suhu,
-//       lembab: req.body.lembab,
-//       analog: req.body.analog,
-//       waktu: new Date(),
-//       status: 'POST data sukses!'
-//   })
-// })
 
 app.listen(PORT, () => {
   console.log(`Listening on ${ PORT }`)
